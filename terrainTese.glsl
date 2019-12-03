5f2a00de4ccb0a593bd60177c8d498e63f1be048
Alissonsz <alisson_tecsouza@hotmail.com>
2019-12-03 02:51:06 -0300

    Update issues

--- terrainTese.glsl
+++ terrainTese.glsl
@@ -95,7 +95,7 @@ void main(){
   
   f00Position.y += fbm(f00Position.xyz*0.1) ;
   
-  //f00Position.y = heightNormal.r*6;
+  f00Position.y = heightNormal.r*0.26;
 
   gl_Position =  vec4(f00Position, 1.0);
 }
